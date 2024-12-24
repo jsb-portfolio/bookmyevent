a3063d483795938a68e8f7ef48787e135c5dbc19
pyro-42 <dev.h8671@slmails.com>
2024-12-23 21:57:03 -0800

    fixed google oauth callback uri to env value

--- src/utils/lucia/auth.ts
+++ src/utils/lucia/auth.ts
@@ -1,6 +1,5 @@
 import { GitHub, Google } from 'arctic'
 
-
 if (!process.env.GITHUB_CLIENT_ID || !process.env.GITHUB_CLIENT_SECRET) {
     throw new Error('MISSING Github_Client_ID or Github_Client_Secret')
 }
@@ -9,11 +8,11 @@ export const github = new GitHub(
     process.env.GITHUB_CLIENT_SECRET
 )
 
-if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
-    throw new Error('MISSING Google_Client_ID or Google_Client_Secret')
+if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET || !process.env.GOOGLE_CALLBACK_URI) {
+    throw new Error('MISSING Google_Client_ID or Google_Client_Secret or Google_Callback_URI')
 }
 export const google = new Google(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
-    'http://localhost:3000/api/auth/google/callback'
+    process.env.GOOGLE_CALLBACK_URI
 )
